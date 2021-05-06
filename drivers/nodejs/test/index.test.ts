b0c9618b99f194557e9928c49ad38cff93c00d28
Alex Kwak <take-me-home@kakao.com>
2021-05-06 13:26:50 +0900

    fix: (driver/nodejs) enable eslint.

--- drivers/nodejs/test/index.test.ts
+++ drivers/nodejs/test/index.test.ts
@@ -1,31 +1,31 @@
-import {types, Client, QueryResultRow} from "pg";
-import {setAGETypes} from "../src";
+import { types, Client, QueryResultRow } from 'pg'
+import { setAGETypes } from '../src'
 
 const config = {
-    user: 'postgres',
-    host: '127.0.0.1',
-    database: 'postgres',
-    password: 'postgres',
-    port: 25432,
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'postgres',
+  password: 'postgres',
+  port: 25432
 }
 
-const testGraphName = 'age-test';
+const testGraphName = 'age-test'
 
-describe("Pre-connected Connection", () => {
-    let client: Client | null;
+describe('Pre-connected Connection', () => {
+  let client: Client | null
 
-    beforeAll(async () => {
-        client = new Client(config);
-        await client.connect();
-        await setAGETypes(client, types);
-        await client.query(`SELECT create_graph('${testGraphName}');`);
-    });
-    afterAll(async () => {
-        await client?.query(`SELECT drop_graph('${testGraphName}', true);`);
-        await client?.end();
-    })
-    it("simple CREATE & MATCH", async () => {
-        await client?.query(`
+  beforeAll(async () => {
+    client = new Client(config)
+    await client.connect()
+    await setAGETypes(client, types)
+    await client.query(`SELECT create_graph('${testGraphName}');`)
+  })
+  afterAll(async () => {
+    await client?.query(`SELECT drop_graph('${testGraphName}', true);`)
+    await client?.end()
+  })
+  it('simple CREATE & MATCH', async () => {
+    await client?.query(`
             SELECT *
             from cypher('${testGraphName}', $$ CREATE (a:Part {part_num: '123'}),
                         (b:Part {part_num: '345'}),
@@ -33,32 +33,32 @@ describe("Pre-connected Connection", () => {
                         (d:Part {part_num: '789'})
                             $$) as (a agtype);
         `)
-        const results: QueryResultRow = await client?.query<QueryResultRow>(`
+    const results: QueryResultRow = await client?.query<QueryResultRow>(`
             SELECT *
             from cypher('${testGraphName}', $$
                 MATCH (a) RETURN a
             $$) as (a agtype);
         `)!
-        expect(results.rows).toStrictEqual(
-            [{
-                "a": {
-                    "id": 844424930131969,
-                    "label": "Part",
-                    "properties": {"part_num": "123"}
-                }
-            }, {
-                "a": {
-                    "id": 844424930131970,
-                    "label": "Part",
-                    "properties": {"part_num": "345"}
-                }
-            }, {
-                "a": {
-                    "id": 844424930131971,
-                    "label": "Part",
-                    "properties": {"part_num": "456"}
-                }
-            }, {"a": {"id": 844424930131972, "label": "Part", "properties": {"part_num": "789"}}}]
-        )
-    });
-});
+    expect(results.rows).toStrictEqual(
+      [{
+        a: {
+          id: 844424930131969,
+          label: 'Part',
+          properties: { part_num: '123' }
+        }
+      }, {
+        a: {
+          id: 844424930131970,
+          label: 'Part',
+          properties: { part_num: '345' }
+        }
+      }, {
+        a: {
+          id: 844424930131971,
+          label: 'Part',
+          properties: { part_num: '456' }
+        }
+      }, { a: { id: 844424930131972, label: 'Part', properties: { part_num: '789' } } }]
+    )
+  })
+})
