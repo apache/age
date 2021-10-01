b7140f1b6391a4592f03c6b66b1cd05f3b944305
Muhammad Shoaib <muhemmed.shoaib@gmail.com>
2021-10-01 16:57:51 +0200

    cleaned tree

--- build.sh
+++ build.sh
@@ -8,6 +8,12 @@ path="$HOME/postgresql/postgresql-$pver/$build/bin"
 
 #REGRESS_OPTS=--encoding=UTF-8 
 
-make PG_CONFIG=$path/pg_config ag_load
+make PG_CONFIG=$path/pg_config clean
+
+make PG_CONFIG=$path/pg_config 
+
+make PG_CONFIG=$path/pg_config install
+
+make PG_CONFIG=$path/pg_config installcheck
 
 find src -type f -name '*\.o' -exec rm {} \;
