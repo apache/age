a794b152a5955c514859e2532c7f2a58512eae4a
Muhammad Shoaib <muhemmed.shoaib@gmail.com>
2021-10-07 13:59:15 +0200

    Alpha Version of ag_load

--- utils/src/include/load/ag_load_labels.h
+++ utils/src/include/load/ag_load_labels.h
@@ -52,13 +52,4 @@ void vertex_row_cb(int delim __attribute__((unused)), void *data);
 int create_labels_from_csv_file(char *file_path, char *graph_name,
                    char *object_name, PGconn *conn );
 
-
-void create_label(PGconn *conn, char* label_name, int label_type);
-
-
-void vertex_field_cb(void *field, size_t field_len, void *data);
-void vertex_row_cb(int delim __attribute__((unused)), void *data);
-
-int create_labels_from_csv_file(char *file_path, char *graph_name,
-                   char *object_name, PGconn *conn );
 #endif //AG_LOAD_LABELS_H
