yugabyte/yugabyte-db

Fix issue #733 - create_complete_graph() terminates the server (#734)
- A vertex and  an edge cannot have the same label within the same
  graph, so added a check for that.

- insert_simple_vertex() and insert_simple_edge() functions were not
  checking for the kind of label. Since a vertex cannot be
  inserted into a label relation of edge and vice versa, this was
  causing the server termination. Added a check for that.

- Added regression tests.

src/backend/utils/load/age_load.c
@@ -148,6 +148,12 @@ void insert_edge_simple(Oid graph_id, char* label_name, graphid edge_id,
     Relation label_relation;
     HeapTuple tuple;
 
+    // Check if label provided exists as vertex label, then throw error
+    if (get_label_kind(label_name, graph_id) == LABEL_KIND_VERTEX)
+    {
+        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+                        errmsg("label %s already exists as vertex label", label_name)));
+    }
 
     values[0] = GRAPHID_GET_DATUM(edge_id);
     values[1] = GRAPHID_GET_DATUM(start_id);
@@ -175,6 +181,13 @@ void insert_vertex_simple(Oid graph_id, char* label_name,
     bool nulls[2] = {false, false};
     Relation label_relation;
     HeapTuple tuple;
+
+    // Check if label provided exists as edge label, then throw error
+    if (get_label_kind(label_name, graph_id) == LABEL_KIND_EDGE)
+    {
+        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+                        errmsg("label %s already exists as edge label", label_name)));
+    }
 
     values[0] = GRAPHID_GET_DATUM(vertex_id);
     values[1] = AGTYPE_P_GET_DATUM((vertex_properties));

apache/age

Conflicts:
	src/backend/utils/graph_generation.c
	src/include/catalog/ag_label.h
Co-authored-by: John Gemignani <jrgemignani@gmail.com>

src/backend/utils/load/age_load.c
@@ -127,6 +127,12 @@ void insert_edge_simple(Oid graph_oid, char *label_name, graphid edge_id,
     Relation label_relation;
     HeapTuple tuple;
 
+    // Check if label provided exists as vertex label, then throw error
+    if (get_label_kind(label_name, graph_oid) == LABEL_KIND_VERTEX)
+    {
+        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+                        errmsg("label %s already exists as vertex label", label_name)));
+    }
 
     values[0] = GRAPHID_GET_DATUM(edge_id);
     values[1] = GRAPHID_GET_DATUM(start_id);
@@ -152,6 +158,13 @@ void insert_vertex_simple(Oid graph_oid, char *label_name, graphid vertex_id,
     bool nulls[2] = {false, false};
     Relation label_relation;
     HeapTuple tuple;
+
+    // Check if label provided exists as edge label, then throw error
+    if (get_label_kind(label_name, graph_oid) == LABEL_KIND_EDGE)
+    {
+        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+                        errmsg("label %s already exists as edge label", label_name)));
+    }
 
     values[0] = GRAPHID_GET_DATUM(vertex_id);
     values[1] = AGTYPE_P_GET_DATUM((vertex_properties));