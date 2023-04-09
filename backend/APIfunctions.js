460220a6a7334834391deb72ed36e0cb25fa203f
KamleshKumar427 <kkumar.bscs20seecs@seecs.edu.pk>
2023-04-09 07:08:04 +0500

    API functions add which needs to be called from frontend to access the backend

---
+++ backend/APIfunctions.js
@@ -0,0 +1,133 @@
+const fetch = require("node-fetch");
+const readline = require("readline");
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+// Test data which will come from the frontend
+const conn = {
+  port: "5432",
+  host: "localhost",
+  password: "ABCDEFGH",
+  user: "kamleshk",
+  dbname: "testdb",
+  ssl: "disable",
+  graph_init: true,
+  version: 11,
+};
+
+// cookies will be stored here, these are store whenever connect() fun is called 
+// and will be used in subsequent calls
+let cookies;
+
+function connect() {
+  fetch('http://localhost:8080/connect', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(conn),
+  })
+  .then(response => {
+    console.log(response.status);
+
+    // Store the cookies from the response
+    cookies = response.headers.raw()['set-cookie'];
+
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+function queryMetadata() {
+  const graph = {
+    name: "demo_graph",
+  };
+
+  fetch('http://localhost:8080/query/metadata', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Cookie': cookies.join('; '),
+    },
+    body: JSON.stringify(graph),
+  })
+  .then(response => {
+    console.log(response.status);
+
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+function query() {
+  const payload = {
+    query: "SELECT * FROM cypher('demo_graph', $$ MATCH (v) RETURN v $$) as (v agtype);",
+  };
+
+  fetch('http://localhost:8080/query', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Cookie': cookies.join('; '),
+    },
+    body: JSON.stringify(payload),
+  })
+  .then(response => {
+    console.log(response.status);
+
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+
+
+
+// dummy function to test the above functions
+
+// let choice;
+// function chooseFunction() {  
+
+// rl.question('Enter the function number: ', (input) => {
+//     const choice = parseInt(input);
+//     if (Number.isNaN(choice) || choice < 1 || choice > 3) {
+//       console.log('Invalid choice. Please try again.');
+//     } else {
+//       switch (choice) {
+//         case 1:
+//           connect();
+//           break;
+//         case 2:
+//           queryMetadata();
+//           break;
+//         case 3:
+//           query();
+//           break;
+//       }
+//     }
+//     // rl.close();
+//     chooseFunction()
+//   });
+
+// }
+
+// chooseFunction();
