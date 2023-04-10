5a6cc983fe5148d329ba2c1d00b326b2417dc42c
moeed-k <99591855+moeed-k@users.noreply.github.com>
2023-04-10 11:16:43 +0500

    update isWriteQuery header

--- src/include/age/age_pool.h
+++ src/include/age/age_pool.h
@@ -24,8 +24,7 @@
 
 bool isCypherQuery (Node* node, char** str);
 void preprocess_cypherstr(char *target);
-List* extractCypherFuncs(List* ParsedCyphertreeList);
-bool IsWriteQuery(List* cypher_funcs_list);
+bool isWriteQuery(List *ParsedCyphertreeList);
 
 
 #endif
