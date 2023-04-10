a534affc5d3e5a913de784c66a671b39dff14aac
Nimra-1234 <72203032+Nimra-1234@users.noreply.github.com>
2023-04-11 03:41:43 +0500

    [AGE-VIEWER-GO]Some necessary links (#797)
    * first page 
    
    firstpage created
    
    * Update App.jsx
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update README.md
    
    * Update main.go

--- age-viewer/main.go
+++ age-viewer/main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "myproject",
+		Title:  "Apache_Age_Viewer",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
