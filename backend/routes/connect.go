460182bcff7b322fa045e0ac981a35dfd3043aca
KamleshKumar427 <kkumar.bscs20seecs@seecs.edu.pk>
2023-04-16 13:07:03 +0500

    Docs added for backend

--- backend/routes/connect.go
+++ backend/routes/connect.go
@@ -3,12 +3,16 @@ package routes
 import (
 	"age-viewer-go/models"
 	"fmt"
-	"net/http"
 
 	"github.com/gorilla/sessions"
 	"github.com/labstack/echo"
 )
 
+/*
+This function takes user data from the request body, establishes a database connection, saves the
+connection information in the session, and returns a JSON response with a "status" field
+set to "connected". It handles errors related to invalid data, connection establishment, and session saving.
+*/
 func ConnectToDb(c echo.Context) error {
 	udata, err := models.FromRequestBody(c)
 	if err != nil {
@@ -33,28 +37,8 @@ func ConnectToDb(c echo.Context) error {
 	return c.JSON(200, map[string]string{"status": "connected"})
 }
 
-func DisconnectFromDb(c echo.Context) error {
-	sess := c.Get("database").(*sessions.Session)
-	dbObj := sess.Values["db"]
-	if dbObj == nil {
-		return echo.NewHTTPError(400, "no database connection found")
-	}
-
-	// Clear cookies
-	for _, cookie := range c.Request().Cookies() {
-		cookie := &http.Cookie{
-			Name:   cookie.Name,
-			Value:  "",
-			Path:   "/",
-			MaxAge: -1,
-		}
-		c.SetCookie(cookie)
-	}
-
-	sess.Values["db"] = nil
-	err := sess.Save(c.Request(), c.Response().Writer)
-	if err != nil {
-		return echo.NewHTTPError(500, "error saving session")
-	}
-	return c.JSON(200, map[string]string{"status": "disconnected"})
-}
+/*
+DisconnectFromDb is used to disconnect from a database by removing the database
+connection object from the user's session  and clearing the cookies. It returns a
+JSON response with a status message indicating that the disconnection was successful.
+*/
