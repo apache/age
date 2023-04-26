b6fd88d5602b2d3f75e928af90a0426d95ce3930
farakh-shahid <farakhraja72@gmail.com>
2023-04-26 19:44:30 +0500

    fmt added

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"embed"
-
+        "fmt"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
