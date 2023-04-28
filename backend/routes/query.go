2df8b20cb1e36e7f884ff869de2af1f180fdc04c
Kamlesh Kumar <95052507+KamleshKumar427@users.noreply.github.com>
2023-04-28 09:19:15 +0500

    [Ageviewer_go_Desktop] Added functionalities to read query response dynamically (#836)
    * Function to disconnect database add to backend
    
    * Added functionalities to read query response dynamically

--- backend/routes/query.go
+++ backend/routes/query.go
@@ -32,19 +32,19 @@ func CypherMiddleWare(next echo.HandlerFunc) echo.HandlerFunc {
 		if err != nil {
 			return echo.NewHTTPError(400, err.Error())
 		}
-		if err = conn.Ping(); err != nil { // check if the connection is still valid
+		if err = conn.Ping(); err != nil {
 			return echo.NewHTTPError(400, err.Error())
 		}
 		if !user.GraphInit {
-			_, err = conn.Exec(db.INIT_EXTENSION) // initialize the AGE extension, if it is not already
+			_, err = conn.Exec(db.INIT_EXTENSION)
 			if err != nil {
 				msg := fmt.Sprintf("could not communicate with db\nerror: %s", err.Error())
 				return echo.NewHTTPError(400, msg)
 			}
-			user.GraphInit = true // set the flag to true, so that the extension is not initialized again
+			user.GraphInit = true
 		}
 		if user.Version == 0 {
-			rows, err := conn.Query(db.PG_VERSION) // get the version of the database, if Version in User connection struct is 0.
+			rows, err := conn.Query(db.PG_VERSION)
 			if err != nil {
 				fmt.Print(err.Error())
 			}
@@ -65,17 +65,15 @@ func CypherMiddleWare(next echo.HandlerFunc) echo.HandlerFunc {
 }
 
 /*
- Cypher function processes Cypher queries in a context object, The function extracts
- the query from the context object, executes it on a database connection, and returns
- the results as a JSON response. It also handles errors that may arise during the query execution.
+Cypher function processes Cypher queries in a context object, The function extracts
+the query from the context object, executes it on a database connection, and returns
+the results as a JSON response. It also handles errors that may arise during the query execution.
 */
-
 func Cypher(ctx echo.Context) error {
 	q := map[string]string{}
-	results := []interface{}{}
 	var err error
 
-	err = ctx.Bind(&q) // extract the query from the request body
+	err = ctx.Bind(&q)
 	if err != nil {
 		e := m.ErrorHandle{
 			Msg: "unable to parse query",
@@ -83,28 +81,22 @@ func Cypher(ctx echo.Context) error {
 		}
 		return ctx.JSON(400, e.JSON())
 	}
-	conn := ctx.Get("conn").(*sql.DB)   // get the database connection from the context object
-	rows, err := conn.Query(q["query"]) // execute the query on the database
-
+	conn := ctx.Get("conn").(*sql.DB)
+	c := make(chan m.ChannelResults, 1)
+	m.CypherCall(conn, q, c)
+	data := <-c
+	err = data.Err
+	res := data.Res
+	msg := data.Msg
 	if err != nil {
 		return echo.NewHTTPError(400, fmt.Sprintf("unable to process query. error: %s", err.Error()))
 	}
 
-	cols, _ := rows.ColumnTypes()
-	if len(cols) == 1 && cols[0].DatabaseTypeName() == "VOID" { // if the query is a DDL query(Data Definition Language), return a 204 status code
-		return ctx.JSON(204, map[string]string{
-			"status": "success",
-		})
+	if _, ok := msg["status"]; ok {
+		return ctx.JSON(204, msg)
 	}
-	for rows.Next() { // iterate over the rows returned by the query
-		data := []any{}
-		err := rows.Scan(&data)
-		if err != nil {
-			print(fmt.Sprintf("\n\nerror: %s", err.Error()))
-		}
-		results = append(results, data)
-	}
-	return ctx.JSON(200, results)
+
+	return ctx.JSON(200, res)
 }
 
 /*
@@ -113,20 +105,22 @@ which graph to query, and the user object to determine the database version. The
 then organized into two lists: one for nodes and one for edges, before being returned as a JSON response.
 */
 func GraphMetaData(c echo.Context) error {
+	dataChan := make(chan *sql.Rows, 1)
+	errChan := make(chan error, 1)
 
-	user := c.Get("user").(models.Connection) // get the user object from the context object
+	user := c.Get("user").(models.Connection)
 	conn := c.Get("conn").(*sql.DB)
 	graph := models.Graph{}
-	c.Bind(&graph) // extract the graph object from the request body
+	c.Bind(&graph)
 	conn.Exec(db.ANALYZE)
-	data, err := graph.GetMetaData(conn, user.Version) // get the metadata for the graph according to the version.
+	go graph.GetMetaData(conn, user.Version, dataChan, errChan)
+	data, err := <-dataChan, <-errChan
 
 	if err != nil {
 		return echo.NewHTTPError(400, err.Error())
 	}
 	results := models.MetaDataContainer{}
 
-	// organize the metadata into two lists: one for nodes and one for edges
 	for data.Next() {
 		row := models.MetaData{}
 		err := data.Scan(&row.Label, &row.Cnt, &row.Kind)
