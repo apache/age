5ee19494b94bedf8fe3fa98817fdbf682319a5f8
Zainab Saad <zsaad.bese21seecs@seecs.edu.pk>
2023-05-05 20:23:37 +0500

    Revert "Resolve merge conflicts"
    This reverts commit 805873fae26d610d9fe62f56f9fc4a32d03c4e59.

--- regress/sql/expr.sql
+++ regress/sql/expr.sql
@@ -532,10 +532,6 @@ $$) AS (i bigint);
 
 SELECT * FROM cypher('type_coercion', $$
 	RETURN true
-$$) AS (i bigint);
-
-SELECT * FROM cypher('type_coercion', $$
-	RETURN true
 $$) AS (i int);
 
 SELECT * FROM cypher('type_coercion', $$
