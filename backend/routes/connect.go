26abb4343c4dd60daccc86992304d40d0343da3f
Sarthak <sarthak.8858@gmail.com>
2023-05-09 21:01:00 +0530

    update function to match web viewer response

--- backend/routes/connect.go
+++ backend/routes/connect.go
@@ -4,7 +4,6 @@ import (
 	"age-viewer-go/models"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/sessions"
 	"github.com/labstack/echo"
@@ -12,8 +11,10 @@ import (
 
 /*
 This function takes user data from the request body, establishes a database connection, saves the
-connection information in the session, and returns a JSON response with a "status" field
-set to "connected". It handles errors related to invalid data, connection establishment, and session saving.
+connection information in the session, and it returns a JSON response with fields
+containing the host, postgres version, port, database, user, password, list of graphs 
+and current graph. It handles errors related to invalid data, connection establishment, and session saving.
+
 */
 func ConnectToDb(c echo.Context) error {
 	udata, err := models.FromRequestBody(c)
@@ -30,35 +31,14 @@ func ConnectToDb(c echo.Context) error {
 	}
 	defer db.Close()
 	sess := c.Get("database").(*sessions.Session)
+	udata.Graphs = []string{}
 	sess.Values["db"] = udata
 	err = sess.Save(c.Request(), c.Response().Writer)
 	if err != nil {
 		return echo.NewHTTPError(400, "could not save session")
 	}
-	
-	type connectionInfo struct {
-		Host     string
-		Version  int
-		Port     int
-		Database string
-		User     string
-		Password string
-		Graphs   []string
-	}
 
-	portInt, _ := strconv.Atoi(udata.Port)
-
-	response := connectionInfo{
-		Host:     udata.Host,
-		Version:  udata.Version,
-		Port:     portInt,
-		Database: udata.DbName,
-		User:     udata.User,
-		Password: udata.Password,
-		Graphs:   []string{},
-	}
-
-	return c.JSON(200, response)
+	return c.JSON(200, udata)
 }
 
 /*
