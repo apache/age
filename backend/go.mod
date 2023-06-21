b94f24ba562980480b940f0b7bd083dace31610a
farakh-shahid <45315010+farakh-shahid@users.noreply.github.com>
2023-06-21 13:19:40 +0500

    Add date and timestamp to log output in logging middleware (#973)

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gorilla/sessions v1.2.1
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/lib/pq v1.10.7
+	github.com/sirupsen/logrus v1.9.3
 )
 
 require (
--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	m "age-viewer-go/miscellaneous"
 	"age-viewer-go/models"
 	"age-viewer-go/routes"
 	"age-viewer-go/session"
 	"encoding/gob"
+	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	log "github.com/sirupsen/logrus"
 )
 
-// main is the entry point for the Backend, it starts the server, and sets up the routes.
 func main() {
 	app := echo.New()
 
@@ -26,8 +28,57 @@ func main() {
 	app.GET("/status", routes.StatusDB)
 	app.POST("/disconnect", routes.DisconnectFromDb)
 	cypher := app.Group("/query", routes.CypherMiddleWare)
-	cypher.Use(m.ValidateContentTypeMiddleWare)
+	cypher.Use(validateContentTypeMiddleware)
 	cypher.POST("/metadata", routes.GraphMetaData)
 	cypher.POST("", routes.Cypher)
+	app.Use(loggingMiddleware)
+
 	app.Start(":8080")
+}
+
+func validateContentTypeMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		contentType := c.Request().Header.Get(echo.HeaderContentType)
+		if contentType != "application/json" {
+			return echo.NewHTTPError(http.StatusBadRequest, "Invalid content type")
+		}
+		return next(c)
+	}
+}
+
+// loggingMiddleware is a custom middleware function for logging requests.
+func loggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		log.WithFields(log.Fields{
+			"method": c.Request().Method,
+			"path":   c.Request().URL.Path,
+		}).Print("Request")
+
+		// Call the next handler
+		err := next(c)
+
+		statusText := getStatusText(err)
+		logMethod := log.Print
+		if err != nil {
+			logMethod = log.Error
+		}
+
+		logMethod(fmt.Sprintf("Response: %s", statusText))
+
+		return err
+	}
+}
+func getStatusText(err error) string {
+	if err != nil {
+		return fmt.Sprintf("Error: %v", err)
+	}
+	return "Success"
+}
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		ForceColors: true,
+	})
+
+	log.SetOutput(os.Stdout)
 }
