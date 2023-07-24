a39e515d94ca2f017f098d649f3393ff9adc3e70
John Gemignani <jrgemignani@gmail.com>
2023-07-24 11:22:53 -0700

    Update files to reflect master branch
    Updated the following files to reflect being on the master branch,
    PostgreSQL version 14.
    
        modified:   META.json
        modified:   RELEASE
    
    Updated the following files for formatting.
    
        modified:   src/backend/executor/cypher_utils.c
        modified:   src/backend/parser/cypher_clause.c

--- src/backend/parser/cypher_clause.c
+++ src/backend/parser/cypher_clause.c
@@ -1344,10 +1344,12 @@ static Query *transform_cypher_unwind(cypher_parsestate *cpstate,
         ereport(ERROR,
                 (errcode(ERRCODE_DUPLICATE_ALIAS),
                         errmsg("duplicate variable \"%s\"", self->target->name),
-                        parser_errposition((ParseState *) cpstate, target_syntax_loc)));
+                        parser_errposition((ParseState *) cpstate,
+                                           target_syntax_loc)));
     }
 
-    expr = transform_cypher_expr(cpstate, self->target->val, EXPR_KIND_SELECT_TARGET);
+    expr = transform_cypher_expr(cpstate, self->target->val,
+                                 EXPR_KIND_SELECT_TARGET);
 
     unwind = makeFuncCall(list_make1(makeString("age_unnest")), NIL,
                           COERCE_SQL_SYNTAX, -1);
@@ -3169,7 +3171,8 @@ static List *make_join_condition_for_edge(cypher_parsestate *cpstate,
             args = list_make3(left_id, right_id, entity->expr);
 
             // add to quals
-            quals = lappend(quals, makeFuncCall(qualified_func_name, args, COERCE_EXPLICIT_CALL, -1));
+            quals = lappend(quals, makeFuncCall(qualified_func_name, args,
+                                                COERCE_EXPLICIT_CALL, -1));
         }
 
         /*
@@ -4358,7 +4361,8 @@ static Node *make_qual(cypher_parsestate *cpstate,
 
 
         args = list_make1(entity->expr);
-        node = (Node *)makeFuncCall(qualified_name, args, COERCE_EXPLICIT_CALL, -1);
+        node = (Node *)makeFuncCall(qualified_name, args, COERCE_EXPLICIT_CALL,
+                                    -1);
     }
     else
     {
