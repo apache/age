39a3a6c3ba28d64d9b11cc169dcaaaae740f156f
Ahmar <ahmarzaidi07@gmail.com>
2023-07-28 01:11:17 +0530

    Add new vite proxy for backed api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
