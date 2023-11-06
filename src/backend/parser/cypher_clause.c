77e7af55930678fef375d14781218f2d1127c3bb
Muhammad Taha Naveed <m.taha.naveed27@gmail.com>
2023-11-06 19:37:01 +0500

    Fix issue 1045 - error using path var in WHERE
    - Added cypher_path as an entity(along side cypher_node and
      cypher_relationship) in order for WHERE in MATCH clause to
      be able to reference it.
    
    - Fixed relevant error messages to be more specific.
    
    - Added regression tests.

--- src/backend/parser/cypher_clause.c
+++ src/backend/parser/cypher_clause.c
@@ -3906,6 +3906,13 @@ static transform_entity *transform_VLE_edge_entity(cypher_parsestate *cpstate,
                         errmsg("variable '%s' is for an edge", rel->name),
                         parser_errposition(pstate, rel->location)));
             }
+            else if (entity->type == ENT_PATH)
+            {
+                ereport(ERROR,
+                       (errcode(ERRCODE_DUPLICATE_ALIAS),
+                        errmsg("variable '%s' is for a path", rel->name),
+                        parser_errposition(pstate, rel->location)));
+            }
             else
             {
                 ereport(ERROR,
@@ -4496,9 +4503,13 @@ transform_match_create_path_variable(cypher_parsestate *cpstate,
     /* otherwise, build the expr node for the function */
     else
     {
+        transform_entity *entity;
         expr = (Expr*)makeFuncExpr(build_path_oid, AGTYPEOID, entity_exprs,
                                    InvalidOid, InvalidOid,
                                    COERCE_EXPLICIT_CALL);
+
+        entity = make_transform_entity(cpstate, ENT_PATH, (Node *)path, expr);
+        cpstate->entities = lappend(cpstate->entities, entity);
     }
 
     resno = cpstate->pstate.p_next_resno++;
@@ -4665,6 +4676,13 @@ static Expr *transform_cypher_edge(cypher_parsestate *cpstate,
                 ereport(ERROR,
                        (errcode(ERRCODE_DUPLICATE_ALIAS),
                         errmsg("variable '%s' is for a VLE edge", rel->name),
+                        parser_errposition(pstate, rel->location)));
+            }
+            else if (entity->type == ENT_PATH)
+            {
+                ereport(ERROR,
+                       (errcode(ERRCODE_DUPLICATE_ALIAS),
+                        errmsg("variable '%s' is for a path", rel->name),
                         parser_errposition(pstate, rel->location)));
             }
         }
@@ -4904,6 +4922,13 @@ static Expr *transform_cypher_node(cypher_parsestate *cpstate,
                 ereport(ERROR,
                        (errcode(ERRCODE_DUPLICATE_ALIAS),
                         errmsg("variable '%s' is for a VLE edge", node->name),
+                        parser_errposition(pstate, node->location)));
+            }
+            else if (entity->type == ENT_PATH)
+            {
+                ereport(ERROR,
+                       (errcode(ERRCODE_DUPLICATE_ALIAS),
+                        errmsg("variable '%s' is for a path", node->name),
                         parser_errposition(pstate, node->location)));
             }
         }
