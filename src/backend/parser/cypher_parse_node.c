277c7a897cee87eb847c60fbb9fd8d428308c00e
Dehowe Feng <dehowefeng@gmail.com>
2024-02-06 18:03:38 +0800

    Implement EXISTS Subquery
    Implements a naive version of EXISTS subquery that evaluates the
    underlying subquery.
    
    Add regression tests related to EXISTS
    
    TODO: Implement logic to allow the underlying subquery to constrain
    the queries in the outer scope.
    
    Also fixes a minor typo.

--- src/backend/parser/cypher_parse_node.c
+++ src/backend/parser/cypher_parse_node.c
@@ -59,6 +59,7 @@ cypher_parsestate *make_cypher_parsestate(cypher_parsestate *parent_cpstate)
         cpstate->graph_name = parent_cpstate->graph_name;
         cpstate->graph_oid = parent_cpstate->graph_oid;
         cpstate->params = parent_cpstate->params;
+        cpstate->subquery_where_flag = parent_cpstate->subquery_where_flag;
     }
 
     return cpstate;
@@ -124,7 +125,7 @@ RangeTblEntry *find_rte(cypher_parsestate *cpstate, char *varname)
 }
 
 /*
- * Generates a default alias name for when a query needs on and the parse
+ * Generates a default alias name for when a query needs one and the parse
  * state does not provide one.
  */
 char *get_next_default_alias(cypher_parsestate *cpstate)
