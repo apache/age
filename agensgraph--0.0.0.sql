1d02065a00bfd165577b00f4774393416d864bb4
yjy44 <jy.yun@bitnine.net>
2024-04-25 17:36:18 +0900

    AGE code cleanup

--- agensgraph--0.0.0.sql
+++
@@ -1,7 +0,0 @@
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION agensgraph" to load this file. \quit
-
-CREATE FUNCTION "cypher"(text)
-RETURNS SETOF "record"
-LANGUAGE "c"
-AS 'MODULE_PATHNAME';
