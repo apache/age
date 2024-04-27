61e173ce61376b0a64b9ea786e4b55b3c735f89f
Aneeqa Asif <87367157+AneeqaCodes@users.noreply.github.com>
2024-04-27 13:58:22 +0500

    Update setup.py
    License: Use the correct SPDX identifier for the Apache License 2.0.
    Classifiers: Added classifiers for OS independence, development status, intended audience, and topic.
    Project URLs: Added URLs for documentation, source code, and issue tracker to provide users with easy access to project resources.

--- drivers/python/setup.py
+++ drivers/python/setup.py
@@ -14,27 +14,38 @@
 # under the License.
 
 from setuptools import setup, find_packages
-from age import VERSION 
+from age import VERSION
 
 with open("README.md", "r", encoding='utf8') as fh:
     long_description = fh.read()
 
 setup(
-    name             = 'apache-age-python',
-    version          = '0.0.7',
-    description      = 'Python driver support for Apache AGE',
+    name='apache_age_python',
+    version='0.0.7',
+    description='Python driver support for Apache AGE',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author           = 'Ikchan Kwon, Apache AGE',
-    author_email     = 'dev-subscribe@age.apache.org',
-    url              = 'https://github.com/apache/age/tree/master/drivers/python',
-    download_url     = 'https://github.com/apache/age/releases' ,
-    license          = 'Apache2.0',
-    install_requires = [ 'psycopg2', 'antlr4-python3-runtime==4.11.1'],
-    packages         = ['age', 'age.gen','age.networkx'],
-    keywords         = ['Graph Database', 'Apache AGE', 'PostgreSQL'],
-    python_requires  = '>=3.9',
-    classifiers      = [
-        'Programming Language :: Python :: 3.9'
-    ]
+    author='Ikchan Kwon, Apache AGE',
+    author_email='dev-subscribe@age.apache.org',
+    url='https://github.com/apache/age/tree/master/drivers/python',
+    download_url='https://github.com/apache/age/releases/download/v0.0.7/apache_age_python-0.0.7.tar.gz',
+    license='Apache License, Version 2.0',
+    install_requires=['psycopg2', 'antlr4-python3-runtime==4.11.1'],
+    packages=find_packages(),
+    keywords=['Graph Database', 'Apache AGE', 'PostgreSQL'],
+    python_requires='>=3.9',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    project_urls={
+        'Documentation': 'https://apache-age.readthedocs.io/en/latest/',
+        'Source': 'https://github.com/apache/age/tree/master/drivers/python',
+        'Tracker': 'https://github.com/apache/age/issues'
+    }
 )
