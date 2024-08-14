b44a8392441b228f3aa605b4a54766c15e1356aa
Muhammad Taha Naveed <mtaha@apache.org>
2024-08-14 23:34:04 +0500

    Revamp age csv loader (#2044)
    * Allow 0 as entry_id
    
    - No regression test were impacted by this change.
    
    * Use batch inserts to improve performance
    
    - Changed heap_insert to heap_multi_insert since it is faster than
      calling heap_insert() in a loop. When multiple tuples can be inserted
      on a single page, just a single WAL record covering all of them, and
      only need to lock/unlock the page once.
    
    - BATCH_SIZE is set to 1000, which is the number of tuples to insert in
      a single batch. This number was chosen after some experimentation.
    
    - Change some of the field names to avoid confusion.
    
    * Use sequence for generating ids for edge and vertex
    
    - Sequence is not used if the id_field_exists is true in
      load_labels_from_file function, since the entry id is present in the
      csv.
    
    * Add function to create temporary table for ids
    
    - Created a temporary table and populate it with already generated
      vertex ids. A unique index is created on id column to ensure that
      new ids generated (using entry id from csv) are unique.
    
    * Insert generated ids in the temporary table to enforce uniqueness
    
    - Insert ids in the temporary table and also update index to
      enforce uniqueness.
    - If the entry id provided in the CSV is greater than the current
      sequence value, the sequence value is updated to match the entry ID.
      For example:
      Suppose the current sequence value is 1, and the CSV entry ID is 2.
      If we use 2 but not update the sequence to 2, next time the CREATE
      clause is used, 2 will be returned by sequence as an entry id,
      resulting in duplicate.
    - Update batch functions
    
    * Add functions to create graph and label automatically
    
    - These functions will check existence of graph and label, and create
      them if they don't exist.
    
    * Add regression tests

--- src/include/catalog/ag_label.h
+++ src/include/catalog/ag_label.h
@@ -74,6 +74,8 @@ Oid get_label_relation(const char *label_name, Oid graph_oid);
 char *get_label_relation_name(const char *label_name, Oid graph_oid);
 Oid get_label_oid(const char *label_name, Oid label_graph);
 char get_label_kind(const char *label_name, Oid label_graph);
+char *get_label_seq_relation_name(const char *label_name);
+
 
 bool label_id_exists(Oid graph_oid, int32 label_id);
 RangeVar *get_label_range_var(char *graph_name, Oid graph_oid,
