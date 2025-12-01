92b4fccaed46a9b4c260537239138a02814f2c46
John Gemignani <jrgemignani@gmail.com>
2025-12-01 09:07:02 -0800

    Update CI for BaseDockerizedTest
    Updated the CI for BaseDockerizedTest.

--- drivers/jdbc/lib/src/test/java/org/apache/age/jdbc/BaseDockerizedTest.java
+++ drivers/jdbc/lib/src/test/java/org/apache/age/jdbc/BaseDockerizedTest.java
@@ -52,7 +52,7 @@ public class BaseDockerizedTest {
         String CORRECT_DB_PASSWORDS = "postgres";
 
         agensGraphContainer = new GenericContainer<>(DockerImageName
-            .parse("apache/age:dev_snapshot_master"))
+            .parse("apache/age:dev_snapshot_PG17"))
             .withEnv("POSTGRES_PASSWORD", CORRECT_DB_PASSWORDS)
             .withExposedPorts(5432);
         agensGraphContainer.start();
